95fc3a30250a6874a4d2a5c3966dff9322e37518
zabokaa <sab_fem@gmx.de>
2024-02-07 19:17:04 +0100

    add new_surplus_data to surplus worksheet

--- run.py
+++ run.py
@@ -81,6 +81,15 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def update_surplus_worksheet(data):
+    """
+    add new row to surplus worksheet with the new calculated surplus variable
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 
 def main():
     """
@@ -90,7 +99,9 @@ def main():
     sales_data = [int(num) for num in data]  #another list comprehension
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
     print(new_surplus_data)
+
 
 print("Happy to see you on Love Sandwiches Data Automation!")
 # do not forget to call the main FUNC to call all FUNCS of the programm
